f5efa5ddbdf58ca30e5de406954bd62865bac7a8
fawazbayureksa <fawwazbayureksa@gmail.com>
2026-01-04 12:10:23 +0700

    Initial Commit

---
+++ swift-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-app
+//
+//  Created by Fawwaz Bayureksa on 04/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-app/swift_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_appApp.swift
+//  swift-app
+//
+//  Created by Fawwaz Bayureksa on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
