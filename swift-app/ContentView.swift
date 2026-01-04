2c086dd62e20624d05758822deb81252530684ea
fawazbayureksa <fawwazbayureksa@gmail.com>
2026-01-04 12:45:46 +0700

    first commit

--- swift-app/ContentView.swift
+++ swift-app/ContentView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:20) {
+            Text("\(count)") // call variable count on text
+            HStack(spacing: 20) {
+                Button(action: {count-=1}){
+                    Text("-")
+                        .font(.largeTitle)
+                        .frame(width: 60,height: 60)
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        .clipShape(Circle())
+                }
+                Button(action:{count+=1}){
+                    Text("+")
+                        .font(.largeTitle)
+                        .frame(width: 60,height: 60)
+                        .background(Color.green)
+                        .foregroundColor(Color.white)
+                        .clipShape(Circle())
+                }
+            }
+            Text("App Counter!").font(.largeTitle)
         }
         .padding()
     }
